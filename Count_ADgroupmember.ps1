c045e4aaa8e159f9c302a5da364b97ebdad87880
saw-hash <73696641+saw-hash@users.noreply.github.com>
2022-01-11 17:40:49 +0800

    Delete Count_ADgroupmember.ps1

--- Count_ADgroupmember.ps1
+++
@@ -1,4 +0,0 @@
-﻿#prompt input
-$group=read-host -Prompt 'Enter group names to add to count'
-#$groups =  Get-ADgroup -Filter * | Select-Object -Property Name| Out-GridView -PassThru -Title 'Select the Groups you want to add the users' | Select-Object -ExpandProperty Name
-(Get-ADGroupMember $group |select Name).count
