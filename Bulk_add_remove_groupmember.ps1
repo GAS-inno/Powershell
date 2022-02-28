3c384a64749e2b6968d514063c8d203ed5da9ab3
saw-hash <73696641+saw-hash@users.noreply.github.com>
2022-02-28 13:33:58 +0800

    Add files via upload

---
+++ Bulk_add_remove_groupmember.ps1
@@ -0,0 +1,115 @@
+﻿#Store the data from ADUsers.csv in the $ADUsers variable  # column header name "username"
+
+
+write-host "`n=============================================================" 
+write-host "======= ADD and Remove to a Groupmembership ======="-foregroundcolor yellow
+write-host "============================================================="
+
+
+
+#----------Function Index menu-----------------
+function Show-Menu {
+    param (
+        [string]$Title = 'Bluk user Add and Remove to a Group' 
+    )
+    Clear-Host
+    Write-Host "================ $Title ================"
+
+    Write-Host "`n1: Press '1' ADD user to Group"
+    Write-Host "2: Press '2' Remove user from Group"
+    Write-Host "`nQ: Press 'Q' to quit.`n"
+}
+
+do
+ {
+    Show-Menu
+    $selection = Read-Host "Please make a selection"
+
+    switch ($selection)
+    {
+
+    '1' {      write-host  'You chose option #1 =>  ADD user to Group' -ForegroundColor Yellow 
+                             
+                             
+                             #import the file                                      
+                           $Import = Import-csv "C:\temp\Passwords_04-02-2022_16027419.csv"
+
+
+                            #importing user 
+                           write-host = "importing user"
+                           $getcontent =  Get-Content -Path 'C:\temp\Passwords_04-02-2022_16027419.csv' | ConvertFrom-CSV | Select-Object 'username', email, upn
+                            $getcontent
+                               
+
+                                
+                               $groupname = read-host 'enter the group name'
+
+
+                            #  $getcontent =  Get-Content -Path 'C:\temp\Passwords_04-02-2022_16027419.csv' | ConvertFrom-CSV | Select-Object username, email, upn
+                            $getcontent
+
+                              #validate Yes/No
+                        $validation = Read-Host -Prompt "`n Do you want to run above script? `nPlease confirm the import user file is correct? [y/n]"
+                                
+                                
+                                if ( $validation -match "[yY]" ) { 
+                                    
+                                     #----Add to group
+
+                                $Import | ForEach-Object {add-ADGroupMember -Identity $groupname -Members $_.username
+                                Get-ADUser -Identity $_.username
+                                write-host (" $($_.username))  |added to $groupname `n ") -ForegroundColor Green } 
+
+
+                                    
+                                }
+                    
+                
+                 }
+   
+    '2' {      write-host 'You chose option #2 =>  Add Multiple user list from CSV.' -ForegroundColor Yellow
+
+                    #read the content of the script file
+                   #$getcontent =  Get-Content -Path '.\Desktop\script_csv_template\'
+                   
+                    #write-host = " $getcontent" -ForegroundColor Green
+
+                        $validation = Read-Host -Prompt "`n Do you want to run above script? [y/n]"
+                                if ( $validation -match "[yY]" ) { 
+                                    # Highway to the danger zone 
+                                     &'.\Bulk_create_user__modify.ps1'
+                                }
+
+                                write-host 'added  user'
+
+            } 
+
+    }
+  
+   pause   #press enter to continue 
+   
+ }
+ until ($selection -eq 'q')
+
+<# 
+$Import = Import-csv ".\Documents\add_distributionMember.csv""
+$groupname = "vngdacchanoi"
+
+
+ #----Add to group
+
+$Import | ForEach-Object {add-ADGroupMember -Identity $groupname -Members $_.PrimarySmtpAddress
+Get-ADUser -Identity $_.SamAccountName
+write-host (" $($_.name)  |added to $groupname `n ") -ForegroundColor Green } 
+
+
+
+ #-----remove from group member
+
+<#
+$Import | ForEach-Object {remove-ADGroupMember -Identity $groupname -Members $_.SamAccountName -Confirm:$False
+Get-ADUser -Identity $_.SamAccountName -Properties extensionAttribute2 | Set-ADUser -Clear "extensionAttribute2"
+write-host (" $($_.name)  |removed from $groupname `n ") -ForegroundColor Green } 
+#>
+
+ 
---
+++ Copy_Paste_Membership.ps1
@@ -0,0 +1,48 @@
+﻿# import the Active Directory module in order to be able to use get-ADuser and Add-AdGroupMembe cmdlet
+import-Module ActiveDirectory
+
+
+
+# enter login name of the first user
+$copy = Read-host "Enter username to copy from: "
+
+# enter login name of the second user
+$paste  = Read-host "Enter username to copy to: "
+
+# copy-paste process. Get-ADuser membership     | then selecting membership                       | and add it to the second user
+get-ADuser -identity $copy -properties memberof | select-object memberof -expandproperty memberof | Add-AdGroupMember -Members $paste
+
+
+
+$yn = read-host "do you want to check the result [y/n]?"
+
+if ($yn -eq 'y'){
+            $sourceUser = get-aduser $paste -Properties memberof 
+
+            $sourceGroups = $sourceUser.memberof 
+
+            foreach($group in $sourceGroups) {
+   
+                                    #split the Array by comma "," | "="
+            $thisgroup = $group.split(",")[0].split("=")[1]
+            $thisGroup
+
+        }
+
+    else { read-host "press enter to exi"}
+
+
+    }
+
+#display result
+
+<#
+Write-Host $User = Get-ADUser -Identity $copy -Properties *;
+$GroupMembership = ($user.memberof | % { (Get-ADGroup $_).Name; }) -join ';';
+write-host "$User.SamAccountName + ';' + $GroupMembership; "  -ForegroundColor green
+
+
+write-host $User = Get-ADUser -Identity $paste -Properties *;
+$GroupMembership = ($user.memberof | % { (Get-ADGroup $_).Name; }) -join ';';
+write-host "$User.SamAccountName + ';' + $GroupMembership; "  -ForegroundColor green
+#>
