c25ae1b7b073efd2daba46815f40510e4449e681
Saw Gwe Aw <73696641+GAS-inno@users.noreply.github.com>
2025-02-28 14:26:47 +0800

    Add files via upload

---
+++ AD_PowerShell/CreateADuser/1_T_ADD_MultipleUser_to_Multiple_ADGroup_byUPN_V2.1_split_group.ps1
@@ -0,0 +1,51 @@
+﻿#group split with ';' in csv
+
+#$ErrorActionPreference = "SilentlyContinue"
+
+
+#$file = Import-CSV "C:\Users\wagasaw\OneDrive - INGENICO Group\powershell\TaskSchedularFiles\myproj\csv\AD_Activation_Date.csv"
+$rootpath = "c:\temp"
+$csvfiles = import-csv -Path (Get-ChildItem -Path "$rootpath\powershellscript\csv"  -Filter '*.csv').FullName | Where-Object {($_.group2 -ne '') -and ($_.action -eq 'addmember' -or $_.action -eq 'create')}
+
+$csvfiles | ForEach-Object {
+     
+  
+
+
+     #  Try{
+            #   Write-host "Checking $($_.upn) in $($_.group2)..........  " -ForegroundColor cyan
+                              
+               #ForEach ($user in $file) {
+                    # Retrieve UPN
+                   $email = $_.upn #whatever your field name
+                   $groups = $_.group2
+         
+                    # Retrieve UPN related SamAccountName
+                   $ADUser = Get-ADUser -Filter "mail -eq '$email'" | Select-Object SamAccountName,name
+
+                foreach ($group in $groups.Split(';')) {
+
+                        $check2 = Get-ADGroupMember -Identity $group | Get-ADUser -Properties * | select Name, samaccountName, userPrincipalName
+
+                    if ($check2.userPrincipalName -contains $_.upn){
+                        write-host "$($_.upn) is a member of $group !" -ForegroundColor red
+                            }
+
+                    else {
+
+                       Add-ADGroupMember -Identity $group -Members $ADUser.SamAccountName -ErrorAction SilentlyContinue
+                      # remove-ADGroupMember -Identity $group -Members $ADUser.SamAccountName  -Confirm:$false
+                        write-host "$($_.upn) is successfully added to $group !" -ForegroundColor green
+                        }
+                       
+                     } #END foreach loop
+ 
+            }
+#} #main *csf file loop end
+  <# catch{
+                Write-Warning "Exception String: $($_.Exception.Message)"
+                }
+
+                #>
+
+#}
---
+++ AD_PowerShell/CreateADuser/1_T_ADD_MultipleUser_to_Singel_DLGroup_byUPN_V1_taskschedular.ps1
@@ -0,0 +1,60 @@
+﻿
+
+$Logfile = "C:\Users\wagasaw\OneDrive - INGENICO Group\powershell\TaskSchedularFiles\myproj\log_script.log"
+function WriteLog
+{
+Param ([string]$LogString)
+$Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+$LogMessage = "$Stamp $LogString"
+Add-content $LogFile -value $LogMessage
+}
+
+
+$scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+& $scriptDir\Exchange_Online_Powershell_Access.ps1
+
+
+#Start-Transcript -Append "C:\Users\wagasaw\OneDrive - INGENICO Group\powershell\TaskSchedularFiles\log_script.log"
+
+#$file = Import-CSV "C:\Users\wagasaw\OneDrive - INGENICO Group\powershell\TaskSchedularFiles\myproj\csv\AD_Activation_Date.csv"
+
+$rootpath = "c:\temp"
+$csvfiles = import-csv -Path (Get-ChildItem -Path "$rootpath\powershellscript\csv"  -Filter '*.csv').FullName | Where-Object {($_.group2 -ne '') -and ($_.action -eq 'addmemember' -or $_.action -eq 'create')}
+
+#import csv file and assigned variable to $file
+       
+        
+ #import csv file and assigned variable to $file
+#$file = Import-CSV "C:\Users\wagasaw\OneDrive - INGENICO Group\powershell\TaskSchedularFiles\myproj\csv\AD_Activation_Date.csv"# "C:\Users\wagasaw\OneDrive - INGENICO Group\powershell\TaskSchedularFiles\add_azureADGroupMember_anygroupname.csv"
+       
+        
+ #import csv file and assigned variable to $file
+       
+    $csvfiles | ForEach-Object{     
+ #Foreach ($user in $file) { 
+      
+       $check1 =  Get-DistributionGroupMember -Identity $_.group |select name, PrimarySmtpAddress |sort-object name, PrimarySmtpAddress
+             #where-object {$_.PrimarySMTPAddress -eq '$_.User'}
+
+            
+          
+      if ($check1.PrimarySmtpAddress -contains $_.upn){
+        write-host "$($_.upn) is already a member of [ $($_.group) ] !" -ForegroundColor red
+
+          $_.upn |  Out-GridView
+        }
+
+       else{
+               Add-DistributionGroupMember -Identity $_.group -Member $_.upn 
+                #remove-ADGroupMember -Identity $group -Members $ADUser.SamAccountName  -Confirm:$false
+                 write-host "$($_.upn) is successfully added to [ $($_.group) ] !" -ForegroundColor green
+                       
+                             }          
+
+
+ }#end foreach loop
+
+
+
+                    
+                           
