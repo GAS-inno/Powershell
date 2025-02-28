22d09e8b605903f22f242198a3eb1f1f7ef3b991
Saw Gwe Aw <73696641+GAS-inno@users.noreply.github.com>
2025-02-28 14:10:14 +0800

    Update bitlocker_get-recoveryKey.ps1

--- bitlocker_get-recoveryKey.ps1
+++ bitlocker_get-recoveryKey.ps1
@@ -1,3 +1,3 @@
-﻿$objComputer = Get-ADComputer SGMYANMARNB1
+﻿$objComputer = Get-ADComputer myhostname
 $Bitlocker_Object = Get-ADObject -Filter {objectclass -eq 'msFVE-RecoveryInformation'} -SearchBase $objComputer.DistinguishedName -Properties 'msFVE-RecoveryPassword'
 $Bitlocker_Object
